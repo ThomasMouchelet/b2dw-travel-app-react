07c1465cc618ac18730862e53c83c16880458998
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2024-11-06 11:56:51 +0100

    logger

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import CommentController from "./comment/comment.controller";
 import TravelController from "./travel/travel.controller";
+import LoggerService from "./middleware/logger.middleware";
 
 const app = express();
 const port = 8000;
@@ -16,6 +17,7 @@ app.use(
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello !");
 });
+app.use(LoggerService);
 
 app.use("/comments", CommentController);
 app.use("/travels", TravelController);
